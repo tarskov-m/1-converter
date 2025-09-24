e9665148cfe8d0eca40e4515fc3cd02fc4e03f34
TarskovMV <maximtarskov@yandex.ru>
2025-09-24 09:14:58 +0500

    Добавлена функция считавания ввода пользователем и пустая функция расчета

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 	const UsdToEur = 0.84
 	const UsdToRub = 84.02
 	const EurToRub = UsdToRub / UsdToEur
+
+	usd := userInput()
+	fmt.Println(usd)
 }
+
+func userInput() float64 {
+	var usd float64
+	fmt.Print("Введите сумму USD: ")
+	fmt.Scanln(&usd)
+
+	return usd
+}
+
+func calculate(usd float64, currency1 string, currency2 string) {}
